b6ec58658dce04759d449afea83ac41bac006444
Luna-Zhang <35479830+Luna-Zhang@users.noreply.github.com>
2021-01-13 20:18:19 +0800

    Add files via upload

---
+++ src/main/java/com/estate/develop/EstateDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.estate.develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstateDevelopApplication.class, args);
+    }
+
+}
